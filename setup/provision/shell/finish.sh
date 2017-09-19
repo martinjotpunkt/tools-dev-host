d5d9a86b672ec81b7559b629b7e25f46a4d90294
michael.liebhauser <michael.liebhauser@bornholdtlee.de>
2017-09-19 15:47:03 +0200

    Packer and Vagrant are working. Project is just a dummy.

--- setup/provision/shell/finish.sh
+++ setup/provision/shell/finish.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 
+# installing dependencies for project
+/usr/local/bin/composer install -d /var/www/vhosts/dev.local
+sudo php /var/www/vhosts/dev.local/bin/console assets:install --env=prod
+
+# set permissions
 sudo chown -R vagrant:www /var/www;
-sudo service php-fpm restart;
+sudo chown -R www:www /sessiontmp
+sudo chown -R www:www /var/www/vhosts/dev.local
+sudo chmod -R 775 /var/www/vhosts/dev.local
+sudo usermod -aG docker vagrant;
+sudo usermod -aG docker www;
+
+# additional steps
+sudo sed -i s/apache/www/g /etc/php-fpm.d/www.conf
+sudo bash -c 'echo "include /var/www/bridges/*.conf;" > /etc/nginx/conf.d/bridges.conf';
+export EDITOR=vi
+sudo bash -c 'printf "root\nvagrant\n" > /etc/incron.allow';
+sudo bash -c 'printf "/var/www/bridges IN_CREATE,IN_MODIFY,IN_DELETE sudo service nginx reload\n" > /etc/incron.d/bridges.conf';
+
+# restarting services
+sudo systemctl restart php-fpm
+sudo systemctl restart incrond
+sudo systemctl restart docker
--- setup/provision/shell/setup.sh
+++ setup/provision/shell/setup.sh
@@ -1,17 +1,35 @@
 #!/usr/bin/env bash
-sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm;
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm;
-sudo yum install -y php70w-fpm php70w-opcache php70w-json php70w-xml php70w-pdo php70w-cli php70w-posix php70w-intl php70w-pecl-apcu.x86_64 php70w-pecl-xdebug.x86_64;
-mkdir /var/www/projects;
-mkdir /var/www/bridges;
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-sudo yum install docker-engine
-sudo systemctl enable docker.service
+
+# creating folders and set ownership
+sudo mkdir -p /var/www/projects;
+sudo mkdir -p /var/www/bridges;
+sudo mkdir -p /var/www/vhosts/dev.local
+sudo mkdir -p /sessiontmp
+
+sudo chown -R vagrant:vagrant /var/www/vhosts/dev.local
+
+# installing packages
+sudo yum makecache
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+sudo yum install -y php70w-fpm php70w-opcache php70w-json php70w-xml php70w-pdo php70w-cli php70w-posix php70w-intl php70w-pecl-apcu.x86_64 php70w-pecl-xdebug.x86_64 nano yum-utils device-mapper-persistent-data lvm2 incron
+
+# docker
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+sudo systemctl enable docker
 sudo systemctl start docker
+
+# symfony
+sudo mkdir -p /usr/local/bin
+sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
+sudo chmod a+x /usr/local/bin/symfony
+
+# composer
+sudo systemctl restart php-fpm
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar composer
+sudo mv composer /usr/local/bin/
