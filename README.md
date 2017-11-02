2f4cf5f1e5e4b1d16013bc03fe70ca3e99f065e6
michael.liebhauser <michael.liebhauser@bornholdtlee.de>
2017-11-02 15:22:31 +0100

    Cleaned up project and wrote better readme

--- README.md
+++ README.md
@@ -6,27 +6,7 @@
 5. Execute "vagrant up"
 6. This might take a few minutes
 7. Now you can log into the VM with "vagrant ssh"
-8. The web interface is accessible with the IP of the VM (ifconfig)
-9. If you want to shut it down, "exit" out of the VM-terminal and then execute "vagrant halt"
-
-## Known issues
-- If you get a 502 - Bad Gateway after reboot, just execute "sudo systemctl restart php-fpm"
-
-## ToDo
-- Dockpit
-- Write container setup instructions
-
-## Docs
-http://blog.endpoint.com/2014/03/provisioning-development-environment.html
-https://github.com/thasmo/packer.templates
-https://github.com/shiguredo/packer-templates
-http://kappataumu.com/articles/creating-an-Ubuntu-VM-with-packer.html
-http://engineering.cotap.com/post/78783269747/hello-world-using-packer-chef-and-berkshelf-on
-http://www.erikaheidi.com/blog/a-begginers-guide-to-vagrant-getting-your-portable-development-e
-http://blog.endpoint.com/2014/03/provisioning-development-environment_14.html
-http://kvz.io/blog/2013/01/16/vagrant-tip-keep-virtualbox-guest-additions-in-sync/
-https://www.digitalocean.com/community/tutorials/how-to-install-chef-and-ruby-with-rvm-on-a-ubuntu-vps
-http://gettingstartedwithchef.com/first-steps-with-chef.html
-https://github.com/boxcutter/ubuntu/blob/master/ubuntu.json
-https://www.digitalocean.com/community/tutorials/how-to-run-nginx-in-a-docker-container-on-ubuntu-14-04
-https://docs.docker.com/engine/reference/api/docker_remote_api_v1.24/
+8. If you want to shut it down, "exit" out of the VM-terminal and then execute "vagrant halt"
+9. Modify your hosts file and set let portainer.local point to the VM-IP
+10. Now you can access portainer via http://portainer.local in your browser
+11. Create account on landing page and select local docker setup in the next step
--- setup/provision/shell/setup.sh
+++ setup/provision/shell/setup.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
 
-# creating folders and set ownership
-sudo mkdir -p /var/www/projects;
-sudo mkdir -p /var/www/bridges;
-sudo mkdir -p /var/www/vhosts/dev.local
-sudo mkdir -p /sessiontmp
-
-sudo chown -R vagrant:vagrant /var/www/vhosts/dev.local
+sudo mkdir -p /var/projects;
 
 # installing packages
-sudo yum makecache
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-sudo yum install -y php70w-fpm php70w-opcache php70w-json php70w-xml php70w-pdo php70w-cli php70w-posix php70w-intl php70w-pecl-apcu.x86_64 php70w-pecl-xdebug.x86_64 nano yum-utils device-mapper-persistent-data lvm2 incron
+sudo yum makecache
+sudo yum install -y nano yum-utils device-mapper-persistent-data lvm2 php56w php56w-pdo php56w-xml
 
 # docker
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# symfony
-sudo mkdir -p /usr/local/bin
-sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
-sudo chmod a+x /usr/local/bin/symfony
+# docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chown -R vagrant:vagrant /var/projects
 
 # composer
-sudo systemctl restart php-fpm
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar composer
-sudo mv composer /usr/local/bin/
+sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo php -r "unlink('composer-setup.php');"
+
+# restarting services
+sudo systemctl restart docker
+
+sudo sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/London\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
+
+sudo docker run --detach --publish 80:80 --volume /var/run/docker.sock:/tmp/docker.sock:ro --name nginx_proxy jwilder/nginx-proxy
+sudo docker run --detach --publish 9000:9000 --volume /var/run/docker.sock:/var/run/docker.sock --volume /opt/portainer:/data --env VIRTUAL_HOST=portainer.local --name portainer portainer/portainer
+
+ifconfig
+
+echo "Everything done. Pick the IP from above and edit your hosts file. The domain portainer.local should point to the IP above."
