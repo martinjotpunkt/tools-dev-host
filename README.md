ad535e5cd73c3097656b70b059132ca04bd2a5de
michael.liebhauser <michael.liebhauser@bornholdtlee.de>
2017-11-28 11:36:30 +0100

    A few improvements and containerstart on boot

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
-## Setup
+## Setup VM
 1. Download Virtualbox from https://www.virtualbox.org/wiki/Downloads
 2. Download Vagrant from https://www.vagrantup.com/downloads.html
-3. Move this folder to your desired destination
-4. Navigate with CLI to this folder
+3. Move this project to your desired destination
+4. Navigate via CLI to this folder
 5. Execute "vagrant up"
 6. This might take a few minutes
 7. Now you can log into the VM with "vagrant ssh"
 8. If you want to shut it down, "exit" out of the VM-terminal and then execute "vagrant halt"
-9. Modify your hosts file and set let portainer.local point to the VM-IP
+9. Modify your hosts file and let portainer.local point to the VM-IP
 10. Now you can access portainer via http://portainer.local in your browser
 11. Create account on landing page and select local docker setup in the next step
+12. VM is ready for development
+
+### Access container shell
+Use following command via cli
+```bash
+sudo docker exec -ti [container_name or id] bash
+```
+or go on the container in portainer and click on "Console" in the "Container status" area.
+
+### Setup project on VM
+1. Upload project root directory to /var/projects
+2. cd into /var/projects/[projectname]/setup
+3. Execute ```sudo docker-compose up -d```
+4. Follow setup instruction of specific project
+
+### Important notes
+- In development process just start, stop or pause the container. Kill and remove can cause data loss.
+- To prevent data loss download and backup volumes. You can list all volumes from a container in the "container detail" page (Volume page in portainer only shows auto generated volumes)
+- In general the first init of an project container stack should be executed via CLI. Every task following can be done in portainer.
--- setup/provision/shell/setup.sh
+++ setup/provision/shell/setup.sh
@@ -6,7 +6,7 @@ sudo mkdir -p /var/projects;
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 sudo yum makecache
-sudo yum install -y nano yum-utils device-mapper-persistent-data lvm2 php56w php56w-pdo php56w-xml
+sudo yum install -y nano yum-utils device-mapper-persistent-data lvm2
 
 # docker
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -15,21 +15,18 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 # docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 
 sudo chown -R vagrant:vagrant /var/projects
 
-# composer
-sudo php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-sudo php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo php -r "unlink('composer-setup.php');"
+sudo mv ~/devstart /usr/bin/devstart
+sudo chmod +x /usr/bin/devstart
+
+sudo echo "devstart" >> /etc/rc.d/rc.local
 
 # restarting services
 sudo systemctl restart docker
-
-sudo sed -i "s/^;date.timezone =$/date.timezone = \"Europe\/London\"/" /etc/php.ini |grep "^timezone" /etc/php.ini
 
 sudo docker run --detach --publish 80:80 --volume /var/run/docker.sock:/tmp/docker.sock:ro --name nginx_proxy jwilder/nginx-proxy
 sudo docker run --detach --publish 9000:9000 --volume /var/run/docker.sock:/var/run/docker.sock --volume /opt/portainer:/data --env VIRTUAL_HOST=portainer.local --name portainer portainer/portainer
